1bb93361eef71484e68f850c9dd328c26e62b15e
Rodion Chernishov <rchernyshov@micfinanceplus.ru>
2025-11-05 15:40:01 +0300

    update projectOCM

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,9 +8,10 @@ def test_login_fail(login_page):
 
 
 @pytest.mark.parametrize('username, password', [
-    ('user', 'password'),
-    ('user', 'password')
+    ('ger@fgdgf.ftg', '123456'),
+    ('+7 (907) 645-21-31', '123456')
 ])
 def test_login_success(login_page, username, password):
     login_page.open_login_page()
     login_page.login(username, password)
+    login_page.page.wait_for_url("https://rc.dev.oneclickmoney.ru/account/")
