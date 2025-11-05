c5520e4c926c98ef2e5d6dbfb9b2e9cec318cc4b
Rodion Chernishov <rchernyshov@micfinanceplus.ru>
2025-11-05 13:11:06 +0300

    update projectOCM

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,1 +1,23 @@
 from playwright.sync_api import Page
+
+class LoginPage(Page):
+    def __init__(self, page: Page):
+        self.page = page
+        self.username_input = page.locator('#loginform-username')
+        self.password_input = page.locator('#loginform-password')
+        self.login_button = page.locator('#idx_login')
+        self.error_message = page.locator('.error-page-field')
+
+
+    def open_login_page(self):
+        self.page.goto('https://rc.dev.oneclickmoney.ru/#signin')
+
+
+    def login(self, username: str, password: str):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+
+    def get_error_message(self):
+        return self.error_message.inner_text()
